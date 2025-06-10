01f4ab4a5ce725efca486798bb039558e6e52520
ammar <ammarbinanwarfuad@gmail.com>
2025-06-10 21:05:55 +0600

    data types cleared

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" //treat all JS code as newer version
+
+// alert("hello");
+// alert(3+3); we are using node js not browser
+
+console.log(3+3)
+
+//code readability should be high
+
+console.log("Ammar")
+
+let name = "Ammar"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/* Primitive DataTypes */
+// number => 2 to power 53
+// big int
+// string => ""
+// boolean = true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof "ammar");
+console.log(typeof age);
+console.log(typeof null); // shows type obj
+console.log(typeof undefined);//shows type undefined
+
