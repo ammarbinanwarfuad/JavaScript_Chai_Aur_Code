4ed9a2c40cd65592f74cdb5cd14a4475c56fcc0a
ammar <ammarbinanwarfuad@gmail.com>
2025-06-10 21:34:41 +0600

    Operations Completed

--- 01_basics/03_conversionOperatio.js
+++ 01_basics/03_conversionOperatio.js
@@ -6,31 +6,31 @@ let score4 = undefined
 let score5 = true
 let score6 = "Ammar"
 
-console.log(typeof score);
-console.log(typeof (score1));
+// console.log(typeof score);
+// console.log(typeof (score1));
 
 let valueInNumber = Number(score2)
-console.log(typeof valueInNumber); // type Number
-console.log(valueInNumber);// output NAN
+// console.log(typeof valueInNumber); // type Number
+// console.log(valueInNumber);// output NAN
 
 
 
 let valueInNumber2= Number(score3)
-console.log(typeof valueInNumber2)
-console.log(valueInNumber2);
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2);
 
 let valueInNumber3= Number(score4)
-console.log(typeof valueInNumber3)
-console.log(valueInNumber3);
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3);
 
 
 let valueInNumber4= Number(score5)
-console.log(typeof valueInNumber4)
-console.log(valueInNumber4);
+// console.log(typeof valueInNumber4)
+// console.log(valueInNumber4);
 
 let valueInNumber5= Number(score6)
-console.log(typeof valueInNumber5)
-console.log(valueInNumber5);
+// console.log(typeof valueInNumber5)
+// console.log(valueInNumber5);
 
 
 /*
@@ -44,13 +44,13 @@ let isLoggedIn1 = ""
 let isLoggedIn2 = "Ammar"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 let booleanIsLoggedIn1 = Boolean(isLoggedIn1);
-console.log(booleanIsLoggedIn1)
+// console.log(booleanIsLoggedIn1)
 
 let booleanIsLoggedIn2 = Boolean(isLoggedIn2);
-console.log(booleanIsLoggedIn2)
+// console.log(booleanIsLoggedIn2)
 
 
 // 1=> true; 0=> false
@@ -59,6 +59,58 @@ console.log(booleanIsLoggedIn2)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
+
+// *********Operations********** 
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(typeof negValue);
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3**3);
+// console.log(3/3);
+// console.log(3%3);
+
+
+let str1 = "hello"
+let str2 = " Ammar"
+
+let str3 = str1+str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(3+4*5%3);
+
+
+// console.log(true);
+// console.log(+true);
+// // console.log(true+);
+// console.log(+"");
+
+let num1,num2,num3;
+
+num1= num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
 
