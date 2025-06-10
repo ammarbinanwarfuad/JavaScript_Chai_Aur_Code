6ce2c37dbc8700c8596347822b57744dc8731342
ammar <ammarbinanwarfuad@gmail.com>
2025-06-10 20:46:25 +0600

    Let Const Var Completed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553;
+let accountEmail = "ammar@gmail.com"
+var accountPassword = "12345"
+accountCity = "Dhaka"
+let accountState;
+
+accountEmail = "ammargucc@gmail.com"
+accountPassword = "2121212"
+accountCity = "DH"
+
+// accountId = 2
+// console.log(object);
+console.log(accountId);
+/*
+    Prefer not to use Var
+    Because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
